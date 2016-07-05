60503b73c84f4aa854bb696bd44f385a62454d6b
chenji <chenji@chenjideMacBook-Pro.local>
2016-07-05 17:34:57 +0800

    0705版

--- app/scripts/join_us.js
+++ app/scripts/join_us.js
@@ -1,12 +1,18 @@
-$('.join_us').find('a').on('click', function(e){
-	if($(this).hasClass('active')){
-		return;
-	}else{
-		let name = $(this).prop("name");
-		let targetName = name + "-div";
-		$('.about').find('a').removeClass("active");
-		$(this).addClass("active");
-		$('.tab-content').prop("hidden", true);
-		$('#' + targetName).prop("hidden", false);
-	}
+// 左侧导航栏点击事件
+$('.join_us ul li a').on('click', function(e){
+  if($(this).hasClass('active')){
+    return;
+  }else{
+    let name = $(this).prop("name");
+    let targetName = name + "-div";
+    $('.join_us').find('a').removeClass("active");
+    $(this).addClass("active");
+    $('.tab-content').prop("hidden", true);
+    $('#' + targetName).prop("hidden", false);
+  }
+});
+
+// 人才招聘toggle按钮事件
+$('.join_us .toggle-icon a').on('click', function(){
+  $(this).parents(".job-title").next().slideToggle();
 });
